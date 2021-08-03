b06ae0e0ef0cdc6e4ef55b5542819f196e3a0a8a
Filip Gulan <filip.gulan@mavenir.com>
2021-08-03 11:28:44 +0200

    pupeteer

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromiumHeadless'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
